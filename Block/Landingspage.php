72739d6595205ef41c5925101943b9f20ad07947
Your Name <you@example.com>
2017-04-19 10:25:33 +0700

    add comment

--- Block/Landingspage.php
+++ Block/Landingspage.php
@@ -1,38 +1,71 @@
 <?php
+
 namespace Magenest\ImageGallery\Block;
+
 use Magento\Framework\View\Element\Template;
+
+/**
+ * Class Landingspage
+ * @package Magenest\ImageGallery\Block
+ */
 class Landingspage extends Template
 {
     private $_imageCollectionFactory;
 
+    protected $_galleryCollectionFactory;
 
-    public function __construct(Template\Context $context, array $data = [],
+    /**
+     * Landingspage constructor.
+     * @param Template\Context $context
+     * @param array $data
+     * @param \Magenest\ImageGallery\Model\ImageFactory $imageCollectionFactory
+     * @param \Magenest\ImageGallery\Model\GalleryFactory $galleryCollectionFactory
+     */
+    public function __construct(
+    Template\Context $context, array $data = [],
     \Magenest\ImageGallery\Model\ImageFactory $imageCollectionFactory,
     \Magenest\ImageGallery\Model\GalleryFactory $galleryCollectionFactory
-    )
-    {
-
+    ) {
         parent::__construct($context, $data);
         $this->_imageCollectionFactory = $imageCollectionFactory;
-        $this->_galleryCollectionFactory=$galleryCollectionFactory;
+        $this->_galleryCollectionFactory =$galleryCollectionFactory;
     }
 
+    /**
+     * @return string
+     */
     public function getLandingsUrl()
     {
         return $this->getUrl('imagegallery');
     }
+
+    /**
+     * @return string
+     */
     public function getRedirectUrl()
     {
         return $this->getUrl('imagegallery/index/gallery');
     }
+
+    /**
+     * @return mixed
+     */
     public function getLoadImageCollection()
     {
         $collection=$this->_imageCollectionFactory->create()->getCollection();
+
         return $collection;
     }
+
+    /**
+     * load collection gallery
+     *
+     * @return mixed
+     */
     public function getLoadGalleryCollection()
     {
         $collection=$this->_galleryCollectionFactory->create()->getCollection();
+
         return $collection;
     }
 
--- Model/Gallery.php
+++ Model/Gallery.php
@@ -1,6 +1,27 @@
 <?php
+
 namespace Magenest\ImageGallery\Model;
-class Gallery extends \Magento\Framework\Model\AbstractModel {
+
+/**
+ * Class Gallery
+ * @package Magenest\ImageGallery\Model
+ * @method int getImageId()
+ * @method string getImage()
+ * @method string getTitle()
+ * @method string getDescription()
+ * @method int getSortorder()
+ * @method string getStatus()
+ */
+class Gallery extends \Magento\Framework\Model\AbstractModel
+{
+    /**
+     * Gallery constructor.
+     * @param \Magento\Framework\Model\Context $context
+     * @param \Magento\Framework\Registry $registry
+     * @param \Magento\Framework\Model\ResourceModel\AbstractResource|null $resource
+     * @param \Magento\Framework\Data\Collection\AbstractDb|null $resourceCollection
+     * @param array $data
+     */
     public function __construct(
         \Magento\Framework\Model\Context $context,
         \Magento\Framework\Registry $registry,
@@ -10,9 +31,13 @@ class Gallery extends \Magento\Framework\Model\AbstractModel {
         null,
         array $data = []
     ) {
-        parent::__construct($context, $registry, $resource,
-            $resourceCollection, $data);
-    } public function _construct() {
+        parent::__construct($context, $registry, $resource, $resourceCollection, $data);
+    }
+
+    public function _construct()
+    {
+
     $this->_init('Magenest\ImageGallery\Model\ResourceModel\Gallery');
-}
+
+    }
 }
